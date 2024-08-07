0b1718256a9e5ef732931c14064286dbc7347fa6
TropiiDev <colinbassett28@proton.me>
2024-08-07 18:18:53 -0400

    version 0.0.6
    Add port to env

--- main.py
+++ main.py
@@ -109,4 +109,7 @@ def refresh_token():
     return redirect('/playlists')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True, port=8080)
+  port = os.getenv('port')
+  if port is None:
+    port = 8080
+  app.run(host='0.0.0.0', debug=True, port=int(port))
